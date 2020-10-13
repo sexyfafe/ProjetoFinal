56455cf943cbcfe9a7b11989705461fab06047b4
Necaz <miguelmatosfac@gmail.com>
2020-10-13 18:37:28 +0100

    Changes

--- Assets/Script/Spawn.cs
+++ Assets/Script/Spawn.cs
@@ -1,10 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 public class Spawn : MonoBehaviour
 {
     private float lifetime = 2;
+    [SerializeField] PhotonView myPhotonView;
+
+    void Start()
+    {
+        myPhotonView = GetComponent<PhotonView>();
+        if (myPhotonView.IsMine == false && PhotonNetwork.IsConnected == true)
+            Destroy(this);
+    }
 
     void OnTriggerEnter()
     {
