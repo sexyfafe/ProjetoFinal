1d2f41d390170844469d885c4916fb19cf50a83c
Necaz <miguelmatosfac@gmail.com>
2020-10-15 15:10:09 +0100

    Teste

---
+++ Assets/PhotonViewDisable.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using Photon.Pun;
+
+public class PhotonViewDisable : MonoBehaviour
+{
+    private PhotonView myPhotonView;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myPhotonView = GetComponent<PhotonView>();
+        if (myPhotonView.IsMine == false && PhotonNetwork.IsConnected == true)
+        {
+            Destroy(this);
+        }
+    }
+}
